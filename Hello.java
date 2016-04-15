11a75dbfa0f2326b32493c133d8088899f2811d0
Youngil Lee <youngilxx@naver.com>
2016-04-15 15:26:10 +0900

    Fifth commit

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello{
   public static void main(String[] args){
     System.out.println("Hello, world from Git!");
     System.out.println("Welcome to Gig world!");
+    System.out.println("My name is Youngil Lee!")
   }
 }
